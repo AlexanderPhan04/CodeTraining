306d965377c942d436b6aa77494bfafb85776333
Darlington0433 <quannnd2004@gmail.com>
2025-12-12 16:10:08 +0700

    Add MySQL data retrieval; implement user count display from the database

--- MySQL/79/79.php
+++ MySQL/79/79.php
@@ -1,4 +1,16 @@
 <!-- lấy dữ liệu MySQL -->
 <?php
     include 'connect.php';
+
+    $sql = "SELECT * FROM users";
+    $result = mysqli_query($conn, $sql);
+
+    echo mysqli_num_rows($result); // hàm mysqli_num_rows trả về số dòng kết quả
+    echo "<br>";
+
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_num_rows($result); // lấy số dòng kết quả
+        echo $row;
+        echo "<br>";
+    }
 ?>
