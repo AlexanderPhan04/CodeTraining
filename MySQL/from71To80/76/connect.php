faa0a3bdde8e65e898267ba65eea6d4c51d36111
Darlington0433 <quannnd2004@gmail.com>
2025-12-17 15:52:12 +0700

    Refactor database connection scripts; update credentials and improve connection handling across multiple versions

--- MySQL/from71To80/76/connect.php
+++ MySQL/from71To80/76/connect.php
@@ -1,21 +1,20 @@
 <?php
-    /*để kết nối với DB cần 4 giá trị
+/*để kết nối với DB cần 4 giá trị
     1. tên serve
     2. user name
     3. password
-    4. tên DB*/ 
-    $server = 'localhost';
-    $user = 'root';
-    $password = '';
-    $DB = 'myWeb';
+    4. tên DB*/
+$server = 'localhost';
+$user = 'root';
+$password = 'quan2004';
+$DB = 'myWeb';
 
-    // tạo kết nối 
-    $conn = new mysqli($server, $user, $password, $DB); // hàm mysqli_connect trả về đối tượng kết nối
+// tạo kết nối 
+$conn = new mysqli($server, $user, $password, $DB); // hàm mysqli_connect trả về đối tượng kết nối
 
-    if ($conn) {
-        mysqli_query($conn, "SET NAMES 'UTF8'"); // thiết lập font chữ UTF8 cho kết nối
-        
-    } else {
-        echo "Connection failed: " . mysqli_connect_error(); // hàm trả về lỗi kết nối
-    }
-?>
+if ($conn) {
+    mysqli_query($conn, "SET NAMES 'UTF8'"); // thiết lập font chữ UTF8 cho kết nối
+
+} else {
+    echo "Connection failed: " . mysqli_connect_error(); // hàm trả về lỗi kết nối
+}
---
+++ MySQL/from91To100/91/91.php
@@ -0,0 +1,25 @@
+<!-- câu lệnh INNER JOIN trong mysql -->
+<?php
+    include 'connect.php';
+    /*
+    WHERE = điều kiện lọc dữ liệu
+    AND  = kết hợp nhiều điều kiện
+    OR = một trong các điều kiện đúng
+    LIKE = tìm kiếm theo mẫu
+    ORDER BY = sắp xếp kết quả
+    LIMIT = giới hạn số kết quả trả về
+    */ 
+
+    // câu lệnh INNER JOIN dùng để kết hợp các bảng với nhau dựa trên một điều kiện chung
+    $sql = "SELECT * FROM users ORDER BY RAND() LIMIT 2";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0)
+    {
+        while ($row = mysqli_fetch_array($result)) {
+            echo $row['id'] . "|" . $row['username'] . "|" . $row['password'] . "|" . $row['level'];
+            echo "<br>";
+        }
+    }
+
+?>
