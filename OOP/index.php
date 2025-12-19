92d3166307c5950329a6d5b627bd3e8d58fad810
Darlington0433 <quannnd2004@gmail.com>
2025-12-19 12:58:55 +0700

    Add initial implementation of object-oriented programming concepts; create Animal class and document traditional vs object-oriented programming

---
+++ OOP/index.php
@@ -0,0 +1,21 @@
+<?php
+    // lớp động vật
+    class Animal
+    {
+        public $name;
+        public $act;
+        public $corlor;
+    }
+    // // chó là động vật
+    // $dog = new Animal();
+    // $dog->name = "gold";
+    // $dog->act = "bark";
+    // $dog->corlor = "black";
+
+    // echo $dog->name . "|";
+    // echo $dog->act;
+
+    $cat = new Animal();
+    $cat->name = "Moon";
+    $cat->act = "meow";
+?>
